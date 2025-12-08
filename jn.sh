7f48ffc472bfef74c3693222fdaaf911cdae3758
joereynolds <joereynolds952@gmail.com>
2025-12-08 09:20:13 +0000

    Consolidate create_note

--- jn.sh
+++ jn.sh
@@ -5,8 +5,24 @@ notes_location=~/Documents/work
 notes_prefix=$(date +%Y-%m-%d)
 notes_suffix=".md"
 
+find_provider_fd_list_notes() {
+    fd . $(get_notes_location) --type file
+}
+
+get_notes_location() {
+    echo $notes_location
+}
+
 get_editor() {
     echo $EDITOR
+}
+
+get_config_dir() {
+    echo $XDG_CONFIG_HOME
+}
+
+list_notes() {
+    find_provider_fd_list_notes
 }
 
 list_books() {
@@ -15,17 +31,16 @@ list_books() {
 }
 
 create_note() {
-    nice_name=$(echo "$1" | tr ' ' '-')
+    if [ $# -eq 1 ]; then
+        nice_name=$(echo "$1" | tr ' ' '-')
+    elif [ $# -eq 2 ]
+    then
+        nice_name=$(echo $2)
+    fi
+
     file_path=$notes_location/$notes_prefix-$nice_name$notes_suffix
 
     echo "$1" > $file_path
-    _post_creation_notice "$file_path"
-}
-
-create_note_with_title() {
-    nice_name=$(echo $2)
-    file_path=$notes_location/$notes_prefix-$nice_name$notes_suffix
-    echo $1 > $file_path
     _post_creation_notice "$file_path"
 }
 
@@ -38,25 +53,21 @@ fuzzy_provider() {
 }
 
 jn__config() {
-    $(get_editor) $XDG_CONFIG_HOME/jn/config.sh
+    $(get_editor) $(get_config_dir)/jn/config.sh
 }
-
 
 jn() {
     if [ "$1" = "config" ]; then
         jn__config
+    elif [ "$1" = "ls" ]; then
+        list_notes
     elif [ $# -eq 0 ]
     then
         list_books
-    elif [ $# -eq 1 ]
+    elif [ $# -gt 0 ]
     then
-        create_note "$1"
-    elif [ $# -eq 2 ]
-    then
-        echo 'here'
-        create_note_with_title "$1" "$2"
+        create_note "$@"
     fi
-
 }
 
 jn "$@"
