59f3fe68fc5ac55a1008fd30dd85fb3a9b223693
Joe Reynolds <joereynolds952@gmail.com>
2026-06-01 19:02:27 +0100

    Get multi working on star command

--- src/subcommands/star.nim
+++ src/subcommands/star.nim
@@ -1,4 +1,4 @@
-import std/[cmdline, dirs, os, parsecfg, paths, strutils, symlinks]
+import std/[cmdline, dirs, os, parsecfg, paths, sequtils, strutils, sugar, symlinks]
 
 import ../config
 import ../fuzzy/fuzzy
@@ -17,14 +17,13 @@ proc process*(config: Config) =
     query
   )
 
-  choice.stripLineEnd()
+  let choices = choice.split("\0")
+                      .map(c => c.strip())
+                      .filterIt(it != "")
 
-  if choice == "":
-    quit()
+  for choice in choices:
+    let filename = extractFilename(choice)
+    createSymlink(Path(choice), Path(getNotesPath(config) & "starred" & DirSep & filename))
 
-  let message = "Starred file " & choice
-  let filename = extractFilename(choice)
-
-  success(message)
-
-  createSymlink(Path(choice), Path(getNotesPath(config) & "starred" & DirSep & filename))
+    let message = "Starred file " & choice
+    success(message)
