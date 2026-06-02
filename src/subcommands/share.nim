86f949e81a95870b89bd8fcf952ec9bd887b3eb8
Joe Reynolds <joereynolds952@gmail.com>
2026-06-02 16:12:17 +0100

    Don't spam paste.rs

--- src/subcommands/share.nim
+++ src/subcommands/share.nim
@@ -11,6 +11,9 @@ proc process*(config: Config, query: string) =
     query
   )
 
+  choice.stripLineEnd()
+  if choice == "": quit()
+
   let choices = choice.split("\0")
                       .map(c => c.strip())
                       .filterIt(it != "")
