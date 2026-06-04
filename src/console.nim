f12c5750ca83e0f18330ed8a5e002607f69892e5
Joe Reynolds <joereynolds952@gmail.com>
2026-06-04 08:16:22 +0100

    Fix colours showing in piped output

--- src/console.nim
+++ src/console.nim
@@ -1,11 +1,14 @@
 import std/[envvars, terminal]
 
+proc canDisplayColour(): bool =
+  return existsEnv("NO_COLOR") or not stdout.isatty()
+
 proc plain*(message: string) =
   echo message
 
 proc success*(message: string) =
 
-  if existsEnv("NO_COLOR"):
+  if canDisplayColour():
     plain(message)
     return
   
@@ -13,21 +16,21 @@ proc success*(message: string) =
 
 proc info*(message: string) =
 
-  if existsEnv("NO_COLOR"):
+  if canDisplayColour():
     plain(message)
     return
 
   stdout.styledWriteLine(fgYellow, message)
 
 proc dim*(message: string) =
-  if existsEnv("NO_COLOR"):
+  if canDisplayColour():
     stdout.write(message)
     return
 
   stdout.styledWrite({styleDim}, message)
 
 proc warn*(message: string) {.raises: [].} =
-  if existsEnv("NO_COLOR"):
+  if canDisplayColour():
     plain(message)
     return
 
